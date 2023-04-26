8e7fd11da44db55bbf6e7e786b6120bef8535db9
nkhhnh <105964980+nkhhnh@users.noreply.github.com>
2023-04-26 14:59:47 +0700

    Update Bai2.java

--- Bai2.java
+++ Bai2.java
@@ -1,12 +1,15 @@
-package Bai2;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
 public class Bai2 {
     public static void main(String[] args) {
     	try {
-			BufferedReader reader = new BufferedReader(new FileReader("E:\\LuuDuLieuSinhVien\\2001200266_DangTranKhoi_JAVA\\KTGK02-2001200266-DangTranKhoi\\src\\Bai2\\input.txt"));
+		url = new java.net.URL("https://raw.githubusercontent.com/nkhhnh/AH/main/input.txt");
+		java.net.URLConnection uc;
+		uc = url.openConnection(); 
+		reader = new BufferedReader(new InputStreamReader(uc.getInputStream()));
             String[] line1 = reader.readLine().split(" ");
             int n = Integer.parseInt(line1[0]);
             int d = Integer.parseInt(line1[1]);
